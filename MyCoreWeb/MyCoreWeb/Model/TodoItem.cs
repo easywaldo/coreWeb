4d2bea0bd7a281e56127608ccfffebd14d81275c
Lee JiNam <jn.lee@ohmyhotel.com>
2018-08-26 23:32:08 +0900

    모델 , 컨트롤러 및 뷰 추가

---
+++ MyCoreWeb/MyCoreWeb/Model/TodoItem.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCoreWeb
+{
+    public class TodoItem
+    {
+        [BindNever]
+        public string Name { get; set; }
+    }
+}
